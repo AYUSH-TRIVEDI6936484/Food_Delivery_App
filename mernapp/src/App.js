6d822fa1e6e2a97fb131a4656717bbb3f57e8ab6
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2023-12-14 19:26:55 +0530

    creating routes and new screens

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -4,15 +4,21 @@ import Home from "./screens/Home";
 import {
   BrowserRouter as  Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+import Login from "./screens/Login";
 
 function App() {
   return ( 
-    <>
-    <div><Home/></div>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
