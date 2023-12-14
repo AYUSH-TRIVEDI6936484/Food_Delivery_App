6cb39b559da923367061ea228d4e9e4e16a7fac1
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2023-12-14 14:09:37 +0530

    created navbar and installed react-router-dom

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import Home from "./screens/Home";
 // import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as  Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return ( 
