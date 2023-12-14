f0e61ebd49ce6df4b9120054850cb01fc851425b
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2023-12-14 21:44:41 +0530

    new components added

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,21 +1,16 @@
 import "./App.css";
 import Home from "./screens/Home";
 // import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as  Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
 
 function App() {
-  return ( 
+  return (
     <Router>
       <div>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/login" element={<Login/>} />
-          
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
