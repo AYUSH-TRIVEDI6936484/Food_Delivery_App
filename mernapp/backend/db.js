93286ca535b5b03eea8f32aef5b522471a12be93
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2023-12-16 00:45:11 +0530

    adding database to our code

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongouri =
-  "mongodb+srv://its_trivedi7:Ayush0511@cluster0.h6omym6.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://its_trivedi7:Ayush0511@cluster0.h6omym6.mongodb.net/Food_Delivery?retryWrites=true&w=majority";
 // const mongodb = async () => {
 //   await mongoose.connect(mongouri, () => {
 //     console.log("Connected Successfully!!");
@@ -11,15 +11,31 @@ const mongouri =
 // mongoose.connect(connectionString, callback);
 
 // Updated code using promises
-const mongodb = () => {
-  mongoose
-    .connect(mongouri)
-    .then(() => {
-      console.log("Connected to MongoDB");
-      // Your code here
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB:", error);
-    });
+// const mongodb = async () => {
+//   try {
+//     await mongoose.connect(mongouri);
+//     console.log("Connected to MongoDB");
+//     const fetched_data = await mongoose.connection.db.collection("food_items");
+//     fetched_data.find({}).toArray(function (err, data) {
+//       if (err) console.log(err);
+//       else console.log(data);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// };
+const mongodb = async () => {
+  try {
+    await mongoose.connect(mongouri);
+    console.log("Connected to MongoDB");
+
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+
+    const data = await fetched_data.find({}).toArray();
+    console.log();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
+
 module.exports = mongodb;
