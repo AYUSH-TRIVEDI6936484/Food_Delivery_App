e96366cc45add55cd117e79dd87edb46627572cb
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2023-12-16 23:37:00 +0530

    setting up validation-express

--- mernapp/backend/routes/CreateUser.js
+++ mernapp/backend/routes/CreateUser.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../models/User");
-
-router.post("/createuser", async (req, res) => {
-  try {
-    await user.create({
-      name: "Ayush",
-      password: "Ayush123",
-      email: "ayush123@gmail.com",
-      location: "Lucknow",
-    });
-    res.json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false });
+const { body, validationResult } = require("express-validator");
+router.post(
+  "/createuser",
+  [
+    body("email").isEmail(),
+    body("name").isLength({ min: 3 }),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({ errors: errors.array() });
+    }
+    try {
+      await user.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
   }
-});
+);
 module.exports = router;
